d0cc4dcdbdfe54d67bcfd77e4a9cb49df41d753f
tejmar <45456437+tejmar@users.noreply.github.com>
2018-12-05 05:25:16 +1100

    Add files via upload

---
+++ IHbot/modules/sticker.py
@@ -0,0 +1,114 @@
+import hashlib
+import os
+from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram import TelegramError
+from telegram import Update, Bot
+from telegram.ext import CommandHandler, run_async
+from telegram.utils.helpers import escape_markdown
+
+from IHbot import dispatcher
+from IHbot.modules.disable import DisableAbleCommandHandler
+
+@run_async
+def stickerid(update: Update):
+    msg = update.effective_message
+    if msg.reply_to_message and msg.reply_to_message.sticker:
+        update.effective_message.reply_text("Hello " +
+                                            "[{}](tg://user?id={})".format(msg.from_user.first_name, msg.from_user.id)
+                                            + ", The sticker id you are replying is :\n```" + 
+                                            escape_markdown(msg.reply_to_message.sticker.file_id) + "```",
+                                            parse_mode=ParseMode.MARKDOWN)
+    else:
+        update.effective_message.reply_text("Hello " + "[{}](tg://user?id={})".format(msg.from_user.first_name,
+                                            msg.from_user.id) + ", Please reply to sticker message to get id sticker",
+                                            parse_mode=ParseMode.MARKDOWN)
+
+@run_async
+def getsticker(bot: Bot, update: Update):
+    msg = update.effective_message
+    chat_id = update.effective_chat.id
+    if msg.reply_to_message and msg.reply_to_message.sticker:
+        bot.sendChatAction(chat_id, "typing")
+        update.effective_message.reply_text("Hello " + "[{}](tg://user?id={})".format(msg.from_user.first_name,
+                                            msg.from_user.id) + ", Please check the file you requested below."
+                                            "\nPlease use this feature wisely!",
+                                            parse_mode=ParseMode.MARKDOWN)
+        bot.sendChatAction(chat_id, "upload_document")
+        file_id = msg.reply_to_message.sticker.file_id
+        newFile = bot.get_file(file_id)
+        newFile.download('sticker.png')
+        bot.send_document(chat_id, document=open('sticker.png', 'rb'))
+        os.remove("sticker.png")
+    else:
+        bot.sendChatAction(chat_id, "typing")
+        update.effective_message.reply_text("Hello " + "[{}](tg://user?id={})".format(msg.from_user.first_name,
+                                            msg.from_user.id) + ", Please reply to sticker message to get sticker image",
+                                            parse_mode=ParseMode.MARKDOWN)
+
+@run_async
+def kang(bot: Bot, update: Update):
+    msg = update.effective_message
+    user = update.effective_user
+    if msg.reply_to_message and msg.reply_to_message.sticker:
+        file_id = msg.reply_to_message.sticker.file_id
+        kang_file = bot.get_file(file_id)
+        kang_file.download('kangsticker.png')
+        hash = hashlib.sha1(bytearray(user.id)).hexdigest()
+        packname = "a" + hash[:20] + "_by_"+bot.username
+        if msg.reply_to_message.sticker.emoji:
+            sticker_emoji = msg.reply_to_message.sticker.emoji
+        else:
+            sticker_emoji = "🤔"
+        try:
+            bot.add_sticker_to_set(user_id=user.id, name=packname,
+                                   png_sticker=open('kangsticker.png', 'rb'), emojis=sticker_emoji)
+            msg.reply_text("Sticker successfully added to [pack](t.me/addstickers/%s)" % packname,
+                            parse_mode=ParseMode.MARKDOWN)
+        except TelegramError as e:
+            if e.message == "Stickerset_invalid":
+                makepack_internal(msg, user, open('kangsticker.png', 'rb'), sticker_emoji, bot)
+            print(e)
+        os.remove("kangsticker.png")
+    else:
+        msg.reply_text("Please reply to a sticker for me to kang it.")
+
+def makepack_internal(msg, user, png_sticker, emoji, bot):
+    name = user.first_name
+    name = name[:50]
+    hash = hashlib.sha1(bytearray(user.id)).hexdigest()
+    packname = f"a{hash[:20]}_by_{bot.username}"
+    try:
+        success = bot.create_new_sticker_set(user.id, packname, name + "'s kang pack",
+                                             png_sticker=png_sticker,
+                                             emojis=emoji)
+    except TelegramError as e:
+        print(e)
+        if e.message == "Sticker set name is already occupied":
+            msg.reply_text("Your pack can be found [here](t.me/addstickers/%s)" % packname,
+                           parse_mode=ParseMode.MARKDOWN)
+        elif e.message == "Peer_id_invalid":
+            msg.reply_text("Contact me in PM first.", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(
+                text="Start", url=f"t.me/{bot.usename}")]]))
+        return
+
+    if success:
+        msg.reply_text("Sticker pack successfully created. Get it [here](t.me/addstickers/%s)" % packname,
+                       parse_mode=ParseMode.MARKDOWN)
+    else:
+        msg.reply_text("Failed to create sticker pack. Possibly due to blek mejik.")
+
+__help__ = """
+- /stickerid: reply to a sticker to me to tell you its file ID.
+- /getsticker: reply to a sticker to me to upload its raw PNG file.
+- /kang: reply to a sticker to add it to your pack.
+"""
+
+__mod_name__ = "Stickers"
+
+STICKERID_HANDLER = DisableAbleCommandHandler("stickerid", stickerid)
+GETSTICKER_HANDLER = DisableAbleCommandHandler("getsticker", getsticker)
+KANG_HANDLER = DisableAbleCommandHandler("kang", kang)
+
+dispatcher.add_handler(STICKERID_HANDLER)
+dispatcher.add_handler(GETSTICKER_HANDLER)
+dispatcher.add_handler(KANG_HANDLER)
---
+++ IHbot/modules/users.py
@@ -0,0 +1,264 @@
+import html
+from io import BytesIO
+from time import sleep
+from typing import Optional, List
+
+from telegram import TelegramError, Chat, Message
+from telegram import Update, Bot, ParseMode
+from telegram.utils.helpers import mention_html
+from telegram.error import BadRequest, Unauthorized
+from telegram.ext import MessageHandler, Filters, CommandHandler
+from telegram.ext.dispatcher import run_async
+
+import IHbot.modules.sql.users_sql as sql
+from IHbot.modules.helper_funcs.misc import send_to_list
+from IHbot import dispatcher, OWNER_ID, LOGGER, SUDO_USERS
+from IHbot.modules.helper_funcs.filters import CustomFilters
+
+USERS_GROUP = 4
+CHAT_BAN_GROUP = 12
+
+
+def get_user_id(username):
+    # ensure valid userid
+    if len(username) <= 5:
+        return None
+
+    if username.startswith('@'):
+        username = username[1:]
+
+    users = sql.get_userid_by_name(username)
+
+    if not users:
+        return None
+
+    elif len(users) == 1:
+        return users[0].user_id
+
+    else:
+        for user_obj in users:
+            try:
+                userdat = dispatcher.bot.get_chat(user_obj.user_id)
+                if userdat.username == username:
+                    return userdat.id
+
+            except BadRequest as excp:
+                if excp.message == 'Chat not found':
+                    pass
+                else:
+                    LOGGER.exception("Error extracting user ID")
+
+    return None
+
+
+@run_async
+def broadcast(bot: Bot, update: Update):
+    to_send = update.effective_message.text.split(None, 1)
+    if len(to_send) >= 2:
+        chats = sql.get_all_chats() or []
+        failed = 0
+        for chat in chats:
+            try:
+                bot.sendMessage(int(chat.chat_id), to_send[1])
+                sleep(0.1)
+            except TelegramError:
+                failed += 1
+                LOGGER.warning("Couldn't send broadcast to %s, group name %s", str(chat.chat_id), str(chat.chat_name))
+
+        update.effective_message.reply_text("Broadcast complete. {} groups failed to receive the message, probably "
+                                            "due to being kicked.".format(failed))
+
+
+@run_async
+def restrict_group(bot: Bot, update: Update, args: List[str]) -> str:
+    message = update.effective_message  # type: Optional[Message]
+
+    # Check if there is only one argument
+    if not len(args) == 1:
+        message.reply_text("Incorrect number of arguments. Please use `/restrict chat_id`.",
+                           parse_mode=ParseMode.MARKDOWN)
+        return
+
+    chat_id = args[0]
+
+    # Check if chat_id is valid
+    if not chat_id.startswith('-') or chat_id.isdigit():
+        message.reply_text("Invalid chat id! "
+                           "Ensure that the chat id is an integer and you included the '-' sign in the chat id.")
+        return
+
+    # Check if chat_id is in bot database
+    chat_title = sql.get_chatname_by_chatid(chat_id)
+    if chat_title is None:
+        message.reply_text("I can't seem to find the chat in my database. "
+                           "Use /chatlist to obtain a list of chats in my database.")
+        return
+
+    chat_restricted = sql.get_restriction(chat_id)
+    if not chat_restricted:
+        chat_title = html.escape(chat_title)
+
+        sudo_users_list = "<b>My Admins:</b>"
+        for user in SUDO_USERS:
+            name = mention_html(user, bot.get_chat(user).first_name)
+            sudo_users_list += "\n - {}".format(name)
+        
+        try:
+            bot.send_message(chat_id=chat_id,
+                             text="I have been restricted by my admins from this chat. "
+                                  "Request any of my admins to add me to this chat.\n\n"
+                                  "{}".format(sudo_users_list), parse_mode=ParseMode.HTML)
+        except Unauthorized as excp:
+            if excp.message == "Forbidden: bot is not a member of the supergroup chat":
+                message.reply_text("Looks like I'm no longer a part of that chat!")
+                return
+            else:
+                LOGGER.exception("Error while sending message to chat.")
+        
+        bot.leave_chat(chat_id)
+
+        sql.set_restriction(chat_id, chat_title, restricted=True)
+        
+        message.reply_text("Successfully left chat <b>{}</b>!".format(chat_title), parse_mode=ParseMode.HTML)
+            
+        # Report to sudo users
+        restrictor = update.effective_user  # type: Optional[User]
+        send_to_list(bot, SUDO_USERS, "{} has restricted me from being added to the chat <b>{}</b>."
+                     .format(mention_html(restrictor.id, restrictor.first_name), chat_title), html=True)
+                    
+    else:
+        message.reply_text("I'm already restricted from that chat!")
+                
+
+@run_async
+def new_message(bot: Bot, update: Update):  # Leave group when a message is sent in restricted group
+    chat = update.effective_chat  # type: Optional[Chat]
+
+    bot.send_message(chat_id=chat.id,
+                     text="I have been restricted by my admins from this chat! "
+                          "Request any of my admins to add me to this chat.")
+    bot.leave_chat(chat.id)
+
+
+@run_async
+def unrestrict_group(bot: Bot, update: Update, args: List[str]) -> str:
+    message = update.effective_message  # type: Optional[Message]
+    
+    # Check if there is only one argument
+    if not len(args) == 1:
+        message.reply_text("Incorrect number of arguments. Please use `/unrestrict chat_id`.",
+                           parse_mode=ParseMode.MARKDOWN)
+        return
+
+    chat_id = args[0]
+
+    # Check if chat_id is valid
+    if not chat_id.startswith('-') or chat_id.isdigit():
+        message.reply_text("Invalid chat id! "
+                           "Ensure that the chat id is an integer and you included the '-' sign in the chat id.")
+        return
+
+    # Check if chat_id is in bot database
+    chat_title = sql.get_chatname_by_chatid(chat_id)
+    if chat_title is None:
+        message.reply_text("I can't seem to find the chat in my database. "
+                           "Use /chatlist to obtain a list of chats in my database.")
+        return
+    
+    chat_restricted = sql.get_restriction(chat_id)            
+    if chat_restricted:
+        chat_title = html.escape(chat_title)
+        sql.set_restriction(chat_id, chat_title, restricted=False)
+        
+        message.reply_text("Successfully removed all restrictions on the chat <b>{}</b>!"
+                           .format(chat_title), parse_mode=ParseMode.HTML)
+                
+        # Report to sudo users
+        unrestrictor = update.effective_user  # type: Optional[User]
+        send_to_list(bot, SUDO_USERS, "{} has removed my restrictions on the chat <b>{}</b>."
+                     .format(mention_html(unrestrictor.id, unrestrictor.first_name), chat_title), html=True)
+            
+    else:
+        message.reply_text("I'm not restricted from that chat!")
+
+
+@run_async
+def log_user(bot: Bot, update: Update):
+    chat = update.effective_chat  # type: Optional[Chat]
+    msg = update.effective_message  # type: Optional[Message]
+
+    sql.update_user(msg.from_user.id,
+                    msg.from_user.username,
+                    chat.id,
+                    chat.title)
+
+    if msg.reply_to_message:
+        sql.update_user(msg.reply_to_message.from_user.id,
+                        msg.reply_to_message.from_user.username,
+                        chat.id,
+                        chat.title)
+
+    if msg.forward_from:
+        sql.update_user(msg.forward_from.id,
+                        msg.forward_from.username)
+
+
+@run_async
+def chats(bot: Bot, update: Update):
+    all_chats = sql.get_all_chats() or []
+    chatfile = 'List of chats.\n'
+    for chat in all_chats:
+        chat_restricted = sql.get_restriction(chat.chat_id)
+
+        if chat_restricted:
+            chatfile += "{} - ({}) [R]\n".format(chat.chat_name, chat.chat_id)
+        else:
+            chatfile += "{} - ({})\n".format(chat.chat_name, chat.chat_id)
+
+    if any(chat.restricted == True for chat in all_chats):
+        chatfile += "[R] - Restricted chat"
+
+    with BytesIO(str.encode(chatfile)) as output:
+        output.name = "chatlist.txt"
+        update.effective_message.reply_document(document=output, filename="chatlist.txt",
+                                                caption="Here is the list of chats in my database.")
+
+
+def __user_info__(user_id):
+    if user_id == dispatcher.bot.id:
+        return """I've seen them in... Wow. Are they stalking me? They're in all the same places I am... oh. It's me."""
+    num_chats = sql.get_user_num_chats(user_id)
+    return """I've seen them in <code>{}</code> chats in total.""".format(num_chats)
+
+
+def __stats__():
+    return "{} users, across {} chats".format(sql.num_users(), sql.num_chats())
+
+
+def __gdpr__(user_id):
+    sql.del_user(user_id)
+
+
+def __migrate__(old_chat_id, new_chat_id):
+    sql.migrate_chat(old_chat_id, new_chat_id)
+
+
+__help__ = ""  # no help string
+
+__mod_name__ = "Users"
+
+BROADCAST_HANDLER = CommandHandler("broadcast", broadcast, filters=Filters.user(OWNER_ID))
+USER_HANDLER = MessageHandler(Filters.all & Filters.group, log_user)
+CHATLIST_HANDLER = CommandHandler("chatlist", chats, filters=CustomFilters.sudo_filter)
+RESTRICT_GROUP_HANDLER = CommandHandler("restrict", restrict_group, pass_args=True, 
+                                        filters=CustomFilters.sudo_filter)
+UNRESTRICT_GROUP_HANDLER = CommandHandler("unrestrict", unrestrict_group, pass_args=True, 
+                                          filters=CustomFilters.sudo_filter)
+NEW_MESSAGE_HANDLER = MessageHandler(CustomFilters.chat_restricted, new_message)
+
+dispatcher.add_handler(USER_HANDLER, USERS_GROUP)
+dispatcher.add_handler(BROADCAST_HANDLER)
+dispatcher.add_handler(CHATLIST_HANDLER)
+dispatcher.add_handler(RESTRICT_GROUP_HANDLER)
+dispatcher.add_handler(UNRESTRICT_GROUP_HANDLER)
+dispatcher.add_handler(NEW_MESSAGE_HANDLER, CHAT_BAN_GROUP)
