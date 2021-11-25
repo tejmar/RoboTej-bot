200efc082eaa7652793c6b8380787069b03b112e
Chris <git@chris-nz.com>
2021-11-25 15:15:45 +1300

    test monkeypatch for job not running

--- IHbot/modules/rss.py
+++ IHbot/modules/rss.py
@@ -228,9 +228,10 @@ __mod_name__ = "RSS Feed"
 
 job = updater.job_queue
 
-job_rss_set = job.run_once(rss_set, 5)
+# job_rss_set = job.run_once(rss_set, 5)
+rss_set()
 job_rss_update = job.run_repeating(rss_update, interval=60, first=60)
-job_rss_set.enabled = True
+# job_rss_set.enabled = True
 job_rss_update.enabled = True
 
 SHOW_URL_HANDLER = CommandHandler("rss", show_url, pass_args=True)
