bd8f1126d240483f2c36830f0c1270c826e092b6
Chris <git@chris-nz.com>
2021-11-25 16:32:07 +1300

    remove SOME use of deprecated RegexHandler behaviour

--- IHbot/modules/reporting.py
+++ IHbot/modules/reporting.py
@@ -3,7 +3,7 @@ from typing import Optional, List
 
 from telegram import Message, Chat, Update, Bot, User, ParseMode
 from telegram.error import BadRequest, Unauthorized
-from telegram.ext import CommandHandler, RegexHandler, run_async, Filters
+from telegram.ext import CommandHandler, run_async, Filters, MessageHandler
 from telegram.utils.helpers import mention_html
 
 from IHbot import dispatcher, LOGGER
@@ -139,7 +139,7 @@ NOTE: neither of these will get triggered if used by admins
 
 REPORT_HANDLER = CommandHandler("report", report, filters=Filters.chat_type.groups)
 SETTING_HANDLER = CommandHandler("reports", report_setting, pass_args=True)
-ADMIN_REPORT_HANDLER = RegexHandler("(?i)@admin(s)?", report)
+ADMIN_REPORT_HANDLER = MessageHandler(Filters.regex("(?i)@admin(s)?"), report)
 
 dispatcher.add_handler(REPORT_HANDLER, REPORT_GROUP)
 dispatcher.add_handler(ADMIN_REPORT_HANDLER, REPORT_GROUP)
