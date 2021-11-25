9c8bef2e791919c326c929717b3f13ae5285f501
Chris <git@chris-nz.com>
2021-11-25 17:56:21 +1300

    allow bot to converse in PMs

--- IHbot/modules/converse.py
+++ IHbot/modules/converse.py
@@ -134,6 +134,7 @@ def words_are_greeting(msg):
 def converse(bot: Bot, update: Update):
     message = update.effective_message
     if (message.reply_to_message and message.reply_to_message.from_user.id and message.reply_to_message.from_user.id == bot.id) \
+        or message.chat_id == bot.id \
             or words_are_greeting(message.text):
         bot.sendChatAction(update.effective_chat.id, "typing")  # Bot typing before send messages
         try:
